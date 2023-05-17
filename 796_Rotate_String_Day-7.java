f69dd7cbf5859e9255cc24ae4aceee46cffe3fbb
Rahul Thombre <67740640+rahulthombre@users.noreply.github.com>
2023-05-17 20:40:20 +0530

    Update 796_Rotate_String_Day-7.java

--- 796_Rotate_String_Day-7.java
+++ 796_Rotate_String_Day-7.java
@@ -1,6 +1,6 @@
 // Problem :- https://leetcode.com/problems/rotate-string
 
-// Solution :-
+// Solution 1 :-
 
 class Solution {
     public boolean rotateString(String s, String goal) {
@@ -12,5 +12,25 @@ class Solution {
         }
         String s2=s+s;
         return (s2.contains(goal));
+    }
+}
+
+// Solution 2 :-
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()!=goal.length()){
+            return false;
+        }
+        if(s.equals(goal)){
+            return true;
+        }
+        for(int i=0;i<s.length();i++){
+            String left=s.substring(i,s.length());
+            String right=s.substring(0,i);
+            if((left+right).equals(goal)==true){
+                return true;
+            }
+        }
+        return false;
     }
 }
