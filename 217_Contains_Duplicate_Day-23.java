2388ccdca8ad68e923cd36b9aa9bef7e847c9058
Rahul Thombre <67740640+rahulthombre@users.noreply.github.com>
2023-06-08 16:14:32 +0530

    Create 217_Contains_Duplicate_Day-23.java

---
+++ 217_Contains_Duplicate_Day-23.java
@@ -0,0 +1,17 @@
+// problem :- https://leetcode.com/problems/contains-duplicate
+
+// Solution :-
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+         HashMap<Integer,Integer> hm=new HashMap<>();
+         for(int i=0;i<nums.length;i++){
+             if(hm.containsKey(nums[i])){
+                 return true;
+             }else{
+                 hm.put(nums[i],i);
+             }
+         }
+         return false;
+    }
+}
