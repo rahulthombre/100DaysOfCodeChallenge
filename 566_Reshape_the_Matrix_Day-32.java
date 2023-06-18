3724054080b0ee2c49a95ecec697a1f6986f63d0
Rahul Thombre <67740640+rahulthombre@users.noreply.github.com>
2023-06-19 02:13:22 +0530

    Create 566_Reshape_the_Matrix_Day-32.java

---
+++ 566_Reshape_the_Matrix_Day-32.java
@@ -0,0 +1,17 @@
+// Problem :- https://leetcode.com/problems/reshape-the-matrix
+
+// Solution :- 
+
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int n=mat.length,m=mat[0].length;
+        if(r*c != n*m){
+            return mat;
+        }
+        int[][] ans=new int[r][c];
+        for(int i=0;i<r*c;i++){
+            ans[i/c][i%c]=mat[i/m][i%m];
+        }
+        return ans;
+    }
+}
