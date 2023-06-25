3ee099142d9ab63ac7381d3abce2fafd419acc9b
Rahul Thombre <67740640+rahulthombre@users.noreply.github.com>
2023-06-25 15:48:43 +0530

    Create 1641_Count_Sorted_Vowel_Strings_Day-34.java

---
+++ 1641_Count_Sorted_Vowel_Strings_Day-34.java
@@ -0,0 +1,16 @@
+// Problem :- https://leetcode.com/problems/count-sorted-vowel-strings
+
+// Solution :-
+
+class Solution {
+    public int countVowelStrings(int n) {
+        int a=1,b=1,c=1,d=1,e=1;
+        for(int i=1;i<n;i++){
+            a=a+b+c+d+e;
+            b=b+c+d+e;
+            c=c+d+e;
+            d=d+e;
+        }
+        return a+b+c+d+e;
+    }
+}
